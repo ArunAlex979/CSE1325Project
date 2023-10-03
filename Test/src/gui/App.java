3ae432c4e6a26fbf9f847cf23a1cc3c782ab6c25
Tucker Sparkman <txs8197@mavs.uta.edu>
2023-10-03 14:45:59 -0500

    push

--- Test/src/gui/App.java
+++ Test/src/gui/App.java
@@ -3,5 +3,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         String s = "World";
         System.out.printf("Hello %s!",s);
+        System.out.printf("Hello %s!",s);
     }
 }
