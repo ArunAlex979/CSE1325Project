28dd5d3bc9c028f46f99ec0b71fdf9bbf2ffd394
Tucker Sparkman <txs8197@mavs.uta.edu>
2023-10-03 14:52:17 -0500

    push test

--- Test/src/gui/App.java
+++ Test/src/gui/App.java
@@ -2,7 +2,8 @@ package gui;
 public class App {
     public static void main(String[] args) throws Exception {
         String s = "World";
+        String x = "Push Test";
         System.out.printf("Hello %s!",s);
-        System.out.printf("Hello %s!",s);
+        System.out.printf("Hello %s!",x);
     }
 }
