a8b83b0547d8ba7b6cb95e416760296e5a466556
Tucker Sparkman <txs8197@mavs.uta.edu>
2023-10-03 16:37:11 -0500

    Project Setup

--- Test/src/gui/TerminalWindow.java
+++ Test/src/gui/TerminalWindow.java
@@ -1,7 +1,20 @@
 package gui;
 
-public class TerminalWindow {
-    public TerminalWindow(){
-        
+import store.*;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class TerminalWindow extends JFrame{
+    
+    private Store store;
+    private JLabel display;
+    
+    public TerminalWindow(String name){
+        this.store = new Store(name);
     }
+
+    public TerminalWindow(Store store){
+        this.store = store;
+    }
+
 }
