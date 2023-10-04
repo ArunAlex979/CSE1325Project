914362336e196c2e6e843b69e871d92501fbf8a0
Tucker Sparkman <txs8197@mavs.uta.edu>
2023-10-03 23:50:59 -0500

    All Together

--- Test/src/gui/TerminalWindow.java
+++ Test/src/gui/TerminalWindow.java
@@ -15,20 +15,67 @@ public class TerminalWindow extends JFrame{
     private JLabel display;
     private File fileName;
     private JPanel activePanel;
+    private JPanel bootPanel;
+    private JPanel adminPanel;
+    private JPanel customerPanel;
 
     private Toolkit tk = Toolkit.getDefaultToolkit();
     private Image logo = tk.getImage("Test/src/gui/recources/logo.png");
     public TerminalWindow(String name){
-        super(name);
+        
+        // Set up JFrame
+        this.setName(name);
+        this.setIconImage(logo);
 
-        activePanel = new JPanel();
+        // Initialize Panels
+        activePanel   = new JPanel();
+        adminPanel    = new JPanel();
+        bootPanel     = new JPanel();
+        customerPanel = new JPanel();
+
+        //////////////////////////////////////////////////
+        // Admin Panel ///////////////////////////////////
+
+        // Create Fields
+        JButton pastOrdersButton = new JButton("");
+        JButton customerInfoButton = new JButton("");
+        JButton inventoryButton = new JButton("");
+        JButton logoutButton = new JButton("");
+        // Add Listeners
+        adminButton   .addActionListener(event -> onAdminClick());
+        customerButton.addActionListener(event -> onCustomerClick());
+
+        // Add Buttons
+        bootPanel.add(adminButton);
+        bootPanel.add(customerButton);
+
+        //////////////////////////////////////////////////
+        // Boot Panel ////////////////////////////////////
+
+
+        // Create Fields
+        JButton adminButton    = new JButton("Admin");
+        JButton customerButton = new JButton("Customer");
+        
+        // Add Listeners
+        adminButton   .addActionListener(event -> onAdminClick());
+        customerButton.addActionListener(event -> onCustomerClick());
+
+        // Add Buttons
+        bootPanel.add(adminButton);
+        bootPanel.add(customerButton);
+
+        //////////////////////////////////////////////////
+        // Customer Panel ////////////////////////////////
+
+
+        setActivePanel(bootPanel);
+
         this.add(activePanel);
-        loginPanel();
-
         this.setVisible(true);
         this.setSize(800,400);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        // this.setIconImage(logo);
+        
         
     }
 
@@ -39,20 +86,9 @@ public class TerminalWindow extends JFrame{
         
     }
 
-    private void loginPanel(){
+    private void setActivePanel(JPanel panel){
         activePanel.removeAll();
-        activePanel.setLayout(new FlowLayout());
-        
-        // Admin Button
-        JButton adminButton = new JButton("Admin");
-        adminButton.addActionListener(event -> onAdminClick());
-        activePanel.add(adminButton);
-
-        // Customer Button
-        JButton customerButton = new JButton("Customer");
-        customerButton.addActionListener(event -> onCustomerClick());
-        activePanel.add(customerButton);
-    
+        this.activePanel = panel;
     }
 
     protected void onAdminClick(){
@@ -61,8 +97,7 @@ public class TerminalWindow extends JFrame{
         this.repaint();
 
         System.out.println("Admin Button Clicked");
-
-        // activePanel =  
+        
     }
     protected void onCustomerClick(){
         System.out.println("Customer Button Clicked");
