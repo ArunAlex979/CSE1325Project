3dc3301d578b415b4736cceea6be2137f3f09561
Tucker Sparkman <txs8197@mavs.uta.edu>
2023-10-03 19:55:56 -0500

    push

--- Test/src/gui/App.java
+++ Test/src/gui/App.java
@@ -1,9 +1,7 @@
 package gui;
 
-import store.Store;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        Store store = new Store("Icy Delight");
+        TerminalWindow store = new TerminalWindow("Icy Delight");
     }
 }
--- Test/src/gui/TerminalWindow.java
+++ Test/src/gui/TerminalWindow.java
@@ -1,20 +1,70 @@
 package gui;
 
 import store.*;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.image.*;
+
+import java.io.*;
 
 public class TerminalWindow extends JFrame{
     
     private Store store;
     private JLabel display;
-    
+    private File fileName;
+    private JPanel activePanel;
+
+    private Toolkit tk = Toolkit.getDefaultToolkit();
+    private Image logo = tk.getImage("Test/src/gui/recources/logo.png");
     public TerminalWindow(String name){
-        this.store = new Store(name);
+        super(name);
+
+        activePanel = new JPanel();
+        this.add(activePanel);
+        loginPanel();
+
+        this.setVisible(true);
+        this.setSize(800,400);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        // this.setIconImage(logo);
+        
     }
 
     public TerminalWindow(Store store){
+        this(store.name());
+
         this.store = store;
+        
     }
 
+    private void loginPanel(){
+        activePanel.removeAll();
+        activePanel.setLayout(new FlowLayout());
+        
+        // Admin Button
+        JButton adminButton = new JButton("Admin");
+        adminButton.addActionListener(event -> onAdminClick());
+        activePanel.add(adminButton);
+
+        // Customer Button
+        JButton customerButton = new JButton("Customer");
+        customerButton.addActionListener(event -> onCustomerClick());
+        activePanel.add(customerButton);
+    
+    }
+
+    protected void onAdminClick(){
+        // Reset Panel
+        this.remove(activePanel);
+        this.repaint();
+
+        System.out.println("Admin Button Clicked");
+
+        // activePanel =  
+    }
+    protected void onCustomerClick(){
+        System.out.println("Customer Button Clicked");
+    }
 }
